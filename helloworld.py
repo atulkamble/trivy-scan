7130fccfecf6ecaa943d500900a57ceea4e6a57d
Atul Kamble <atul_kamble@hotmail.com>
2026-09-18 09:47:39 +0530

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+print("hello world")
+print("username: admin")
+
+# Basic password comment
+# password: admin
+
+# Dummy secrets for Trivy practice ONLY
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
+
+DATABASE_URL = "mysql://admin:TestPassword123@database.example.com:3306/mydb"
